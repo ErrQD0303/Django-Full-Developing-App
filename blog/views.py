c5ef6da2ff041f419ea97dd68e6ecdf1f2cc0e35
ErrQD0303 <quocdat3396@gmail.com>
2024-05-22 01:28:49 +0700

    Change method-based view into Class-based view

--- blog/views.py
+++ blog/views.py
@@ -1,66 +1,7 @@
-from datetime import date
-from django.shortcuts import render
-from django.http import Http404
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't event prepared for what happened whilst I was enjoying the view!",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, illo similique error perspiciatis cum quo nostrum. Maxime fuga commodi natus, nesciunt nihil, nemo ab, numquam magni enim quasi consectetur tempora?
-            
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, illo similique error perspiciatis cum quo nostrum. Maxime fuga commodi natus, nesciunt nihil, nemo ab, numquam magni enim quasi consectetur tempora?
-            
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, illo similique error perspiciatis cum quo nostrum. Maxime fuga commodi natus, nesciunt nihil, nemo ab, numquam magni enim quasi consectetur tempora?
-        """,
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-]
+from typing import Any
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def get_date(post):
@@ -69,23 +10,54 @@ def get_date(post):
 # Create your views here.
 
 
-def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def posts(request):
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts,
-    })
+# def starting_page(request):
+#     # Django will read the whole line, then making the query to the database, so the query will only take 3 latest date post in the database, so the performance is better, because it only fetches 3 results from the database, and because the [-3:] is not supported by django
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts,
+#     })
 
 
-def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
 
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+# def post_detail(request, slug):
+#     # all_posts = Post.objects.all()
+#     # identified_post = next(
+#     #     post for post in all_posts if post.slug == slug)
+#     identified_post = get_object_or_404(Post, slug=slug)
+
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "post_tags": identified_post.tags.all()
+#     })
